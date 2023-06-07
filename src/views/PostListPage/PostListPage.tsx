bd2953a5a65aa00b2daf7d5f1c00575e5451cf5a
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2023-06-07 14:55:24 +0800

    🆕 PostListPage: пагинация реализована

--- src/views/PostListPage/PostListPage.tsx
+++ src/views/PostListPage/PostListPage.tsx
@@ -1,4 +1,4 @@
-import { Spinner } from 'react-bootstrap';
+import { Button, Form, InputGroup, Pagination, Spinner } from 'react-bootstrap';
 import { useAppDispatch, useAppSelector } from '@/store/store';
 import { useDispatchOnMount } from '@/hooks/useDispatchOnMount';
 import {
@@ -6,15 +6,51 @@ import {
   getPostComments,
 } from '@/store/features/json-placeholder/sagas';
 import PostList from '@/components/PostList';
+import { useState } from 'react';
+
+// https://dev.to/admantium/react-creating-a-custom-hook-for-pagination-jni
+export function usePagination<T>(list: T[], itemsPerPage: number) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const maxPage = Math.ceil(list.length / itemsPerPage);
+
+  function currentData() {
+    const begin = (currentPage - 1) * itemsPerPage;
+    const end = begin + itemsPerPage;
+    return list.slice(begin, end);
+  }
+
+  function next() {
+    setCurrentPage((currentPage) => Math.min(currentPage + 1, maxPage));
+  }
+
+  function prev() {
+    setCurrentPage((currentPage) => Math.max(currentPage - 1, 1));
+  }
+
+  function jump(page: number) {
+    const pageNumber = Math.max(1, page);
+    setCurrentPage(Math.min(pageNumber, maxPage));
+  }
+
+  return { next, prev, jump, currentData, currentPage, maxPage };
+}
 
 function PostListPage() {
   useDispatchOnMount(getAllPosts());
   const dispatch = useAppDispatch();
+  const allPosts = useAppSelector(
+    ({ jsonPlaceholderReducer }) => jsonPlaceholderReducer.allPosts || [],
+  );
+  const {
+    currentData: getPaginatedPostList,
+    next,
+    prev,
+    jump,
+    currentPage,
+    maxPage,
+  } = usePagination(allPosts, 7);
   const commentMap = useAppSelector(
     ({ jsonPlaceholderReducer }) => jsonPlaceholderReducer.commentMap,
-  );
-  const allPosts = useAppSelector(
-    ({ jsonPlaceholderReducer }) => jsonPlaceholderReducer.allPosts,
   );
   const isLoading = useAppSelector(
     ({ jsonPlaceholderReducer }) => jsonPlaceholderReducer.loading,
@@ -23,20 +59,48 @@ function PostListPage() {
     ({ jsonPlaceholderReducer }) => jsonPlaceholderReducer.loadingComments,
   );
 
+  function generatePagination(): React.ReactElement[] {
+    return Array.from(new Array(maxPage)).map((_, index) => {
+      const page = index + 1;
+      return (
+        <Pagination.Item
+          key={page}
+          active={page === currentPage}
+          onClick={() => jump(page)}
+        >
+          {page}
+        </Pagination.Item>
+      );
+    });
+  }
+
   function openPostCommentList(postId: number) {
     dispatch(getPostComments(postId));
   }
+
+  // TODO: поиск по заголовку
+  // TODO: сортировка по заголовку
+  // TODO: пагинация +
 
   if (isLoading && !allPosts) return <Spinner />;
 
   return (
     <section>
+      <InputGroup className='mb-3'>
+        <Form.Control placeholder='Введите заголовок статьи' />
+        <Button variant='outline-secondary'>X</Button>
+      </InputGroup>
       <PostList
-        postList={allPosts || []}
+        postList={getPaginatedPostList()}
         commentMap={commentMap}
         loadingComments={loadingComments}
         openPostCommentList={(postId) => openPostCommentList(postId)}
       />
+      <Pagination>
+        <Pagination.Prev onClick={prev} />
+        {generatePagination()}
+        <Pagination.Next onClick={next} />
+      </Pagination>
     </section>
   );
 }
